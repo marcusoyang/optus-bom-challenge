66b9f4cd792c29d386fa95d6ee1d00da5b807a06
Marcus O'Yang <18moyang@gmail.com>
2022-03-01 01:25:58 +1100

    filtering implemented

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
 const app = require('express')();
 const axios = require('axios');
+const { response } = require('express');
 
-const url = "http://www.bom.gov.au/fwo/IDN60801/IDN60801.95765.json";
+const URL = "http://www.bom.gov.au/fwo/IDN60801/IDN60801.95765.json";
+const FILTER_TEMP = 20;
 
 app.get('/', async (req, res ) => {
+    let highTempStations = [];
+
     try {
-        const response = await axios.get(url);
-        res.json(response.data);
+        const response = await axios.get(URL);
+        const data = response.data["observations"]["data"];
+
+        data.forEach(station => {
+            if (station["apparent_t"] > FILTER_TEMP) {
+                highTempStations.push({
+                    name: station["name"],
+                    apparent_t: station["apparent_t"],
+                    lat: station["lat"],
+                    long: station["lon"]
+                });
+            }
+        });
+        highTempStations.sort((a, b) => parseFloat(a.apparent_t) - parseFloat(b.apparent_t));
+        res.json(highTempStations);
     } catch (error) {
-        res.json({ error: "Error Connecting to BOM."});
+        res.json({
+            status: 503,
+            error: error["message"]
+        });
+        console.log(error);
     }
-
-
-
 });
 
 const port = process.env.PORT || 8080;
