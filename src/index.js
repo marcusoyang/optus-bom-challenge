a1f264cdf9eae55253bd24e3cc0f06d8e945366f
Marcus O'Yang <18moyang@gmail.com>
2022-03-01 02:53:52 +1100

    added unit tests

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 const app = require('express')();
 const axios = require('axios');
 
-const URL = "http://www.bom.gov.au/fwo/IDN60801/IDN60801.95765.jso";
+const URL = "http://www.bom.gov.au/fwo/IDN60801/IDN60801.95765.json";
 const FILTER_TEMP = 20;
 
+const filterStations = (data) => {
+    arr = [];
+    data.forEach(station => {
+        if (station["apparent_t"] > FILTER_TEMP) {
+            arr.push({
+                name: station["name"],
+                apparent_t: station["apparent_t"],
+                lat: station["lat"],
+                long: station["lon"]
+            });
+        }
+    });
+    return arr;
+}
+
 app.get('/', async (req, res ) => {
-    let highTempStations = [];
     try {
         // GET request using Axios
         const response = await axios.get(URL);
-        const data = response.data["observations"]["data"];
-        data.forEach(station => {
-            if (station["apparent_t"] > FILTER_TEMP) {
-                highTempStations.push({
-                    name: station["name"],
-                    apparent_t: station["apparent_t"],
-                    lat: station["lat"],
-                    long: station["lon"]
-                });
-            }
-        });
+        let highTempStations = filterStations(response.data["observations"]["data"]);
         highTempStations.sort((a, b) => parseFloat(a.apparent_t) - parseFloat(b.apparent_t));
         res.json({ response: highTempStations });
     } catch (error) {
@@ -33,5 +37,7 @@ app.get('/', async (req, res ) => {
 });
 
 const port = process.env.PORT || 8080;
+
+module.exports = filterStations;
 
 app.listen(port, () => console.log(`app listening on http://localhost:${port}`) );
