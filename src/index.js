6c6d9868ff43db7954cdb8feefa057286c7ab39e
Marcus O'Yang <18moyang@gmail.com>
2022-03-01 00:27:29 +1100

    implemented get request

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 const app = require('express')();
+const axios = require('axios');
 
-app.get('/', (req, res ) => 
-    res.json({ message: 'Docker is easy 🐳' }) 
-);
+const url = "http://www.bom.gov.au/fwo/IDN60801/IDN60801.95765.json";
+
+app.get('/', async (req, res ) => {
+    try {
+        const response = await axios.get(url);
+        res.json(response.data);
+    } catch (error) {
+        res.json({ error: "Error Connecting to BOM."});
+    }
+
+
+
+});
 
 const port = process.env.PORT || 8080;
 
